f8d9e8b66d45812736a0e6950e5ec301767460e3
Piavlo <lolitushka@gmail.com>
2014-07-15 11:47:47 +0000

    librdkafka

---
+++ librdkafka/recipe.rb
@@ -0,0 +1,25 @@
+class Librdkafka < FPM::Cookery::Recipe
+  homepage    'https://github.com/edenhill/librdkafka'
+  name        'librdkafka'
+  version     '0.8'
+  source      'https://github.com/edenhill/librdkafka', :with => :git, :branch => 'master'
+
+  revision    '1'
+  maintainer  'Piavlo <lolitushka@gmail.com>'
+  license     'BSD'
+  description 'Apache Kafka C/C++ client library'
+  section     'messaging'
+
+  depends       'zlib1g'
+  build_depends 'gcc', 'make', 'zlib1g-dev'
+
+  def build
+    configure \
+      :prefix     => prefix
+    make
+  end
+
+  def install
+    make :install, 'DESTDIR' => destdir
+  end
+end
